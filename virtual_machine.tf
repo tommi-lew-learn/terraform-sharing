6200b7e46da97f7a2ac0d90928763c5d7164de91
Tommi <tommi-lew@users.noreply.github.com>
2022-06-17 14:09:32 +0800

    feat: add aws instance resource for virtual machine example

---
+++ virtual_machine.tf
@@ -0,0 +1,26 @@
+# Doc: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ami
+data "aws_ami" "amazon_linux_2" {
+  # Reference: https://ap-southeast-1.console.aws.amazon.com/ec2/v2/home?region=ap-southeast-1#ImageDetails:imageId=ami-0c802847a7dd848c0
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-x86_64-gp2"]
+  }
+
+  # Reference: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/virtualization_types.html
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"]
+}
+
+# Doc: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "example-ec2"
+  }
+}
