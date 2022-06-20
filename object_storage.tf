28bbc5010a69788d8c8a12828c90e7d475b31e00
Tommi <tommi-lew@users.noreply.github.com>
2022-06-20 20:06:17 +0800

    feat: reintroduce ability to create multiple buckets
    This reverts commit c7d223a1ef5ce29044a3de30b1a52907864af28f.

--- object_storage.tf
+++ object_storage.tf
@@ -1,9 +1,18 @@
+variable "bucket_names" {
+  type = list(string)
+  default = ["apple", "banana", "calamansi"]
+}
+
 resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket-${random_id.name_suffix.hex}"
+  for_each = toset(var.bucket_names)
+
+  bucket = "example-bucket-${random_id.name_suffix.hex}-${each.key}"
 }
 
 resource "aws_s3_bucket_acl" "example_acl" {
-  bucket = aws_s3_bucket.example.id
+  for_each = toset(var.bucket_names)
+  
+  bucket = aws_s3_bucket.example[each.key].id
   acl    = "private"
 }
 
