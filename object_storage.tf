c7d223a1ef5ce29044a3de30b1a52907864af28f
Tommi <tommi-lew@users.noreply.github.com>
2022-06-20 20:03:48 +0800

    feat: remove ability to create multiple buckets

--- object_storage.tf
+++ object_storage.tf
@@ -1,18 +1,9 @@
-variable "bucket_names" {
-  type = list(string)
-  default = ["apple", "banana", "calamansi"]
-}
-
 resource "aws_s3_bucket" "example" {
-  for_each = toset(var.bucket_names)
-
-  bucket = "example-bucket-${random_id.name_suffix.hex}-${each.key}"
+  bucket = "example-bucket-${random_id.name_suffix.hex}"
 }
 
 resource "aws_s3_bucket_acl" "example_acl" {
-  for_each = toset(var.bucket_names)
-  
-  bucket = aws_s3_bucket.example[each.key].id
+  bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
 
